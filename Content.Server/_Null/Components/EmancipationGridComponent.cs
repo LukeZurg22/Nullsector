2a617ea38360e9e0a7ceac404dc08758eca657a3
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-11-06 00:30:36 -0600

    Added Working Emancipation grid

--- Content.Server/_Null/Components/EmancipationGridComponent.cs
+++ Content.Server/_Null/Components/EmancipationGridComponent.cs
@@ -1,24 +1,143 @@
 using Content.Server._Null.Systems;
+using Content.Shared.Construction.Prototypes;
+using Content.Shared.Materials;
 using Content.Shared.Whitelist;
 using Robust.Shared.Audio;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server._Null.Components;
 
 /// <summary>
 /// Designates this entity as a device that acts upon a grid for Emancipation.
+/// <br/><br/>
+/// Made for the Null Sector by LZ22, use this however you please.
 /// </summary>
-[RegisterComponent, Access(typeof(EmancipationGridSystem))/*, NetworkedComponent*/]
+/* # ===USAGE EXAMPLE=== (Customize as you please. The values provided are implicit with the component.)
+ # Careful. There are a LOT of things the Emancipation Grid will delete.
+ - type: EmancipationGrid
+    emancipateSound: # Optional sound effect parameters
+    path: /Audio/Effects/zzzt.ogg
+        params:
+    volume: -3
+    useSound:
+    path: /Audio/Effects/toilet_seat_down.ogg
+        params:
+    volume: -3
+    whitelist: # Objects the emancipation grid WILL delete.
+        tags:
+        - etc
+        components:
+        - etc
+    blacklist: # Objects the emancipation grid WILL delete.
+        tags:
+        - etc
+        components:
+        - etc
+*/
+[RegisterComponent, Access(typeof(EmancipationGridSystem)) /*, NetworkedComponent*/]
 public sealed partial class EmancipationGridComponent : Component
 {
-    [ViewVariables(VVAccess.ReadOnly)] public long ItemsCleaned { get; set; }
+    /// <summary>
+    /// Number of items cleaned thus far. Should be fine to contain within an Int32.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadOnly)] public int ItemsCleaned { get; set; }
 
+    /// <summary>
+    /// Specific Components and Tags that the Emancipation Grid will delete. If empty, it will do nothing.
+    /// This does not need to be set in the prototype, as basic trash is defined as default.
+    /// </summary>
     [ViewVariables(VVAccess.ReadOnly), DataField]
-    public EntityWhitelist? Whitelist { get; set; } = new() { Components = ["SpaceGarbage"], Tags = ["Trash"], };
+    public EntityWhitelist? Whitelist { get; set; } = new()
+    {
+        Components = ["SpaceGarbage", "Puddle"], // The Armada hates spills. No weakness, make no mistakes!
+        Tags = ["Trash"],
+    };
+
+    /// <summary>
+    /// Blacklisted qualities of items that will cause the emancipation grid to ignore them.
+    /// </summary>
+    public EntityWhitelist? Blacklist { get; set; } = new()
+    {
+        Components = ["Hypospray", "Food"],
+        Tags = // I know this thing is pretty brutal, but it doesn't have to be monstrously bad.
+        [
+            "BoxCardboard",
+            "GlassBeaker", "CentrifugeCompatible", "DrinkBottle", "ChemDispensable",
+            "Meat", "Egg",
+        ],
+    };
+
+    /// <summary>
+    /// The current grid that this entity is emancipating of trash.
+    /// </summary>
     [ViewVariables(VVAccess.ReadOnly)] public EntityUid? EmancipatedGrid { get; set; }
+
+    /// <summary>
+    /// Check to see if this device is receiving power. Can be toggled, but it would be unwise due to possible
+    /// unpredicted behaviour.
+    /// </summary>
     public bool IsPowered = false;
+
+    #region Default Audio Values
+
+    /// <summary>
+    /// Path to default soundbyte if one is not specified in the prototype file.
+    /// </summary>
+    public const string DefaultEmancipateSound = "/Audio/Effects/zzzt.ogg";
+
+    public const string DefaultOutputSound = "/Audio/Effects/toilet_seat_down.ogg";
+
+    [ValidatePrototypeId<MaterialPrototype>]
+    public const string BiomassPrototype = "Biomass";
+
+    public static AudioParams DefaultAudioParameters = AudioParams.Default.WithVolume(-3);
+
+    #endregion
 
     /// <summary>
     ///  If not null, this sound will be played when an item is deleted both on the item and the machine.
     /// </summary>
-    [DataField("sound")] public SoundSpecifier? EmancipateSound;
+    [DataField("emancipateSound")] public SoundSpecifier? SoundEmancipate;
+
+    [DataField("useSound")] public SoundSpecifier? SoundUse;
+
+    //-----------------------------------------------------------------------------------------------\\
+    //---------------------------------------Biomass Variables---------------------------------------\\
+    //-----------------------------Lifted from Biomass ReclaimerComponent----------------------------\\
+    // WARN: Warning, there may be an issue regarding upgrades. The code appears sound, but adding matter-
+    //  -bins via R.P.E.D. in testing appears to do nothing? Regardless, be wary. -Z
+
+    /// <summary>
+    /// Amount of biomass that the entity being processed will yield.
+    /// This is calculated from the YieldPerUnitMass.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float CurrentExpectedYield = 0f;
+
+    /// <summary>
+    /// How many units of biomass it produces for each unit of mass.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float YieldPerUnitMass = default;
+
+    /// <summary>
+    /// The base yield per mass unit when no components are upgraded.
+    /// </summary>
+    [DataField("baseYieldPerUnitMass")]
+    public float BaseYieldPerUnitMass = 0.4f;
+
+    /// <summary>
+    /// Machine part whose rating modifies the yield per mass.
+    /// </summary>
+    [DataField("machinePartYieldAmount", customTypeSerializer: typeof(PrototypeIdSerializer<MachinePartPrototype>))]
+    public string MachinePartYieldAmount = "MatterBin";
+
+    /// <summary>
+    /// How much the machine part quality affects the yield.
+    /// Going up a tier will multiply the yield by this amount.
+    /// </summary>
+    [DataField("partRatingYieldAmountMultiplier")]
+    public float PartRatingYieldAmountMultiplier = 1.25f;
+
+    //-----------------------------------------------------------------------------------------------\\
 }
--- Content.Server/_Null/Systems/EmancipationGridSystem.cs
+++ Content.Server/_Null/Systems/EmancipationGridSystem.cs
@@ -1,10 +1,18 @@
 using System.Diagnostics.CodeAnalysis;
 using Content.Server._Null.Components;
+using Content.Server.Construction;
+using Content.Server.Explosion.Components;
+using Content.Server.Materials;
+using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
 using Content.Shared.Examine;
+using Content.Shared.Interaction;
+using Content.Shared.Popups;
 using Content.Shared.Power;
 using Content.Shared.Tag;
+using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Physics.Components;
 
 namespace Content.Server._Null.Systems;
 
@@ -15,6 +23,8 @@ public sealed class EmancipationGridSystem : EntitySystem
 
     [Dependency] private readonly TagSystem _tagSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly MaterialStorageSystem _material = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     public override void Initialize()
     {
@@ -23,57 +33,188 @@ public sealed class EmancipationGridSystem : EntitySystem
         SubscribeLocalEvent<EmancipationGridComponent, ComponentInit>(OnComponentInit);
         SubscribeLocalEvent<EmancipationGridComponent, ComponentShutdown>(OnComponentShutdown);
         SubscribeLocalEvent<EmancipationGridComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<EmancipationGridComponent, RefreshPartsEvent>(OnRefreshParts);
+        SubscribeLocalEvent<EmancipationGridComponent, UpgradeExamineEvent>(OnUpgradeExamine);
+        SubscribeLocalEvent<EmancipationGridComponent, ActivateInWorldEvent>(OnActivate);
+    }
+
+    private void OnRefreshParts(EntityUid uid, EmancipationGridComponent component, RefreshPartsEvent args)
+    {
+        var matterBinRating = args.PartRatings[component.MachinePartYieldAmount];
+
+        // Yield slopes upwards with part rating.
+        component.YieldPerUnitMass =
+            component.BaseYieldPerUnitMass *
+            MathF.Pow(component.PartRatingYieldAmountMultiplier, matterBinRating - 1);
+    }
+
+    private void OnUpgradeExamine(EntityUid uid, EmancipationGridComponent component, UpgradeExamineEvent args)
+    {
+        args.AddPercentageUpgrade("emancipation-grid-component-upgrade-biomass-yield",
+            component.YieldPerUnitMass / component.BaseYieldPerUnitMass);
     }
 
     private void OnExamined(Entity<EmancipationGridComponent> ent, ref ExaminedEvent args)
-        => args.PushMarkup(Loc.GetString("emancipation-grid-examined", ("amount", ent.Comp.ItemsCleaned)));
+    {
+        args.PushMarkup(Loc.GetString("emancipation-grid-examined-amount", ("amount", ent.Comp.ItemsCleaned)));
+        args.PushMarkup(Loc.GetString("emancipation-grid-examined-yield",
+            ("yield", Math.Round(ent.Comp.CurrentExpectedYield, 2))));
+    }
 
     private void OnComponentShutdown(Entity<EmancipationGridComponent> ent, ref ComponentShutdown args)
     {
         ent.Comp.EmancipatedGrid = null;
     }
 
-    private void OnComponentInit(Entity<EmancipationGridComponent> ent, ref ComponentInit args)
+    private bool AreOtherEmancipationGridsPresent(Entity<EmancipationGridComponent> device)
     {
-        ent.Comp.EmancipatedGrid = Transform(ent).GridUid;
-    }
-
-    private void HandlePowerChange(Entity<EmancipationGridComponent> emancipationGrid, ref PowerChangedEvent args)
-    {
-        emancipationGrid.Comp.IsPowered = this.IsPowered(emancipationGrid.Owner, EntityManager);
-        if (!emancipationGrid.Comp.IsPowered)
+        // Checking to ensure there aren't other emancipation grids, else it will simply not work!
+        var emancipationGrids = AllEntityQuery<EmancipationGridComponent>();
+        while (emancipationGrids.MoveNext(out var otherDeviceComponent))
         {
-            Stop(emancipationGrid);
-            return; // Short-circuit
+            if (otherDeviceComponent == device.Comp)
+                continue;
+            if (otherDeviceComponent.EmancipatedGrid != Transform(device).GridUid)
+                continue; // Short-Circuit
+            _popup.PopupEntity(Loc.GetString("emancipation-grid-alone-popup"), device, PopupType.Medium);
+            return true;
         }
 
-        emancipationGrid.Comp.EmancipatedGrid = Transform(emancipationGrid).GridUid; // Set current grid
+        return false;
     }
 
-    private void HandleItemDeletion(EmancipationGridComponent emancipationGridMachine, EntityUid entityToDelete)
+    private void OnComponentInit(Entity<EmancipationGridComponent> device, ref ComponentInit args)
     {
-        emancipationGridMachine.ItemsCleaned++; // Increment cleaned item counter.
+        EnsureComp<TriggerOnActivateComponent>(device);
+
+        // Sound fallbacks
+        device.Comp.SoundEmancipate ??=
+            new SoundPathSpecifier(EmancipationGridComponent.DefaultEmancipateSound,
+                EmancipationGridComponent.DefaultAudioParameters);
+        device.Comp.SoundUse ??=
+            new SoundPathSpecifier(EmancipationGridComponent.DefaultOutputSound,
+                EmancipationGridComponent.DefaultAudioParameters);
+
+        if (AreOtherEmancipationGridsPresent(device))
+        {
+            Stop(device);
+            return;
+        }
+
+        Start(device);
+    }
+
+    private void HandlePowerChange(Entity<EmancipationGridComponent> device, ref PowerChangedEvent args)
+    {
+        device.Comp.IsPowered = this.IsPowered(device.Owner, EntityManager);
+        if (AreOtherEmancipationGridsPresent(device) || !device.Comp.IsPowered)
+        {
+            Stop(device);
+            return;
+        }
+
+        Start(device);
+    }
+
+    private void HandleItemDeletion(EmancipationGridComponent emancipationComponent, EntityUid entityToDelete)
+    {
+        emancipationComponent.ItemsCleaned++; // Increment cleaned item counter.
 
         // Get locations of entity & machine.
         var deletedEntityLocation = Transform(entityToDelete).Coordinates;
-        var machineLocation = Transform(emancipationGridMachine.Owner).Coordinates;
+        var machineLocation = Transform(emancipationComponent.Owner).Coordinates;
 
         // Play sound at Deleted Entity and Emancipation Grid Machine before deletion.
-        Console.WriteLine($"Playing sound \"{emancipationGridMachine.EmancipateSound}\"");
-        if (emancipationGridMachine.EmancipateSound != null)
+        //  EmancipateSound should never be null thanks to Component Initialization defaulting to one.
+        _audio.PlayPvs(emancipationComponent.SoundEmancipate, deletedEntityLocation); // At deleted entity.
+        _audio.PlayPvs(emancipationComponent.SoundEmancipate, machineLocation); // At machine.
+
+        var expectedYield = emancipationComponent.BaseYieldPerUnitMass;
+        if (TryComp<PhysicsComponent>(entityToDelete, out var physics))
         {
-            _audio.PlayPvs(emancipationGridMachine.EmancipateSound, deletedEntityLocation); // At deleted entity.
-            _audio.PlayPvs(emancipationGridMachine.EmancipateSound, machineLocation); // At machine.
+            expectedYield *= physics.FixturesMass;
         }
+
+        emancipationComponent.CurrentExpectedYield += expectedYield;
 
         EntityManager.QueueDeleteEntity(entityToDelete); // Finally delete the entity
     }
 
-    private void Stop(Entity<EmancipationGridComponent> emancipationGridMachine)
+    #region OnActivate / On / Off Methods
+
+    /// <summary>
+    /// Toggles Emancipation Grid device on or off.
+    /// </summary>
+    /// <param name="ent"></param>
+    /// <param name="args"></param>
+    private void OnActivate(Entity<EmancipationGridComponent> ent, ref ActivateInWorldEvent args)
     {
-        emancipationGridMachine.Comp.EmancipatedGrid = null;
-        Dirty(emancipationGridMachine);
+        // Complex Action, skip if user can't do Complex actions.
+        if (args.Handled || !args.Complex)
+            return;
+
+        // Doesn't matter whether it is powered or not, if there are other Emancipation Grids, this turns-off NOW.
+        if (AreOtherEmancipationGridsPresent(ent))
+        {
+            Stop(ent);
+            args.Handled = true;
+            return;
+        }
+
+        // if the device isn't powered, simply turn it on.
+        if (!ent.Comp.IsPowered)
+        {
+            Start(ent);
+            args.Handled = true;
+            return;
+        }
+
+        // If the device is powered, then it may have biomass. If it has biomass, it should prioritize-
+        //  -releasing that biomass rather than disable the device.
+        var actualYield = (int)ent.Comp.CurrentExpectedYield; // Can only have an integer of biomass, for comparisons.
+        if (actualYield == 0) // If it has nothing, then clearly, we must turn this thing off.
+        {
+            Stop(ent);
+            args.Handled = true;
+            return;
+        }
+
+        // Given that it must have some kind of yield, we may instead spawn biomass rather than disable the machine.
+        ent.Comp.CurrentExpectedYield -= actualYield; // store non-integer leftovers
+
+        _material.SpawnMultipleFromMaterial(
+            amount: actualYield,
+            material: EmancipationGridComponent.BiomassPrototype,
+            coordinates: Transform(ent).Coordinates);
+
+        _audio.PlayPvs(ent.Comp.SoundUse, Transform(ent).Coordinates); // Play "Use" sound at machine.
+
+        args.Handled = true;
     }
+
+    /// <summary>
+    /// Boots the Emancipation grid and assigns the Grid ID to its component.
+    /// </summary>
+    /// <param name="device"></param>
+    private void Start(Entity<EmancipationGridComponent> device)
+    {
+        device.Comp.EmancipatedGrid = Transform(device).GridUid; // Set current grid
+        if (TryComp<ApcPowerReceiverComponent>(device, out var power))
+            power.PowerDisabled = false;
+    }
+
+    /// <summary>
+    /// Effectively shuts down the Emancipation Grid.
+    /// </summary>
+    /// <param name="device"></param>
+    private void Stop(Entity<EmancipationGridComponent> device)
+    {
+        device.Comp.EmancipatedGrid = null;
+        if (TryComp<ApcPowerReceiverComponent>(device, out var power))
+            power.PowerDisabled = true;
+    }
+
+    #endregion
 
     [SuppressMessage("ReSharper", "RedundantJumpStatement")]
     public override void Update(float frameTime)
@@ -90,35 +231,63 @@ public sealed class EmancipationGridSystem : EntitySystem
         var emancipationGrids = AllEntityQuery<EmancipationGridComponent>();
         while (emancipationGrids.MoveNext(out var emancipationGridComponent))
         {
-            if (!emancipationGridComponent.IsPowered)
+            if (!emancipationGridComponent.IsPowered || !emancipationGridComponent.EmancipatedGrid.HasValue)
                 continue; // Short-circuit
 
             // Check for entities on grid.
-            var gridEnumerator = Transform(emancipationGridComponent.EmancipatedGrid!.Value).ChildEnumerator;
+            var gridEnumerator = Transform(emancipationGridComponent.EmancipatedGrid.Value).ChildEnumerator;
             Stack<EntityUid> deleteList = [];
 
             // Loop over all possible entities that can be added to a list for deletion.
             while (gridEnumerator.MoveNext(out var gridEntity))
             {
-                // Check all whitelisted components.
-                foreach (var comp in emancipationGridComponent.Whitelist!.Components!)
+                #region Filters
+
+                // Cache lookups
+                var blacklist = emancipationGridComponent.Blacklist;
+                var whitelist = emancipationGridComponent.Whitelist;
+
+                // Blacklist checks
+                if (blacklist is { Components.Length: > 0 })
                 {
-                    // Check if entity has component, and proceed if so.
-                    var componentType = EntityManager.ComponentFactory.GetRegistration(comp).Type;
-                    if (!HasComp(gridEntity, componentType))
-                        continue; // Short-circuit the Foreach Component list.
-                    deleteList.Push(gridEntity);
+                    foreach (var comp in blacklist.Components)
+                    {
+                        var type = EntityManager.ComponentFactory.GetRegistration(comp).Type;
+                        if (HasComp(gridEntity, type))
+                            goto PROCEED; // Immediately skip processing if blacklisted
+                    }
+                }
+
+                if (TryComp<TagComponent>(gridEntity, out var tagComp) &&
+                    _tagSystem.HasAnyTag(tagComp, blacklist?.Tags!))
+                {
                     goto PROCEED;
                 }
-                // Check if entity has tag component, and if that component has any whitelisted tags.
-                if (TryComp<TagComponent>(gridEntity, out var tagComponent) &&
-                    _tagSystem.HasAnyTag(tagComponent, emancipationGridComponent.Whitelist?.Tags!))
+
+                // Whitelist checks
+                if (whitelist is { Components.Length: > 0 })
+                {
+                    foreach (var comp in whitelist.Components)
+                    {
+                        var type = EntityManager.ComponentFactory.GetRegistration(comp).Type;
+                        if (!HasComp(gridEntity, type))
+                            continue; // Short-circuit in foreach loop
+                        deleteList.Push(gridEntity);
+                        goto PROCEED;
+                    }
+                }
+
+                if (TryComp(gridEntity, out tagComp) &&
+                    _tagSystem.HasAnyTag(tagComp, whitelist?.Tags!))
                 {
                     deleteList.Push(gridEntity);
-                    goto PROCEED;
                 }
+
+                #endregion
+
                 PROCEED: ; // Logic that I personally can cope with in the late evening. It works. -Z
             }
+
             // Handle item deletions in bulk.
             var count = deleteList.ToArray().Length;
             for (var index = 0; index < count; index++)
