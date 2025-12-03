3f4c7ee7b8460353b911bc69f2a16818146963a6
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-02 21:38:24 -0600

    Updated ShipyardDirectionSystem.cs

--- Content.Server/_Mono/Shipyard/ShipyardDirectionSystem.cs
+++ Content.Server/_Mono/Shipyard/ShipyardDirectionSystem.cs
@@ -2,6 +2,7 @@ using System.Numerics;
 using Content.Server.Chat.Managers;
 using Content.Shared.Chat;
 using Content.Shared.Localizations;
+using Robust.Server.GameObjects;
 using Robust.Server.Player;
 
 namespace Content.Server._Mono.Shipyard;
@@ -13,23 +14,35 @@ public sealed class ShipyardDirectionSystem : EntitySystem
 {
     [Dependency] private readonly IChatManager _chatManager = default!;
     [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+
+    /// <summary>
+    /// Made for Null Sector by LZ22. I left this in here because it felt fair, and appropriate. -LZ22<br/><br/>
+    /// Sends a message to the player indicating the literal coordinates of their newly purchased hull.
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="hull"></param>
+    public void SendShipLocationMessage(EntityUid player, EntityUid hull)
+    {
+        // Try to get player's and ship's locations.
+        if (!TryGetPositions(player, hull, out _, out var shipPos))
+            return;
+
+        // Send message to player
+        var message = Loc.GetString(
+            "nullith-location-message", // Null Sector - For use with the Nullith. Localized vaguely to accomodate ships, if needed.
+            ("location", shipPos));
+
+        SendMessageToPlayer(player, message);
+    }
 
     /// <summary>
     /// Sends a message to the player indicating the compass direction of their newly purchased ship
     /// </summary>
     public void SendShipDirectionMessage(EntityUid player, EntityUid ship)
     {
-        if (!TryComp<TransformComponent>(player, out var playerTransform) ||
-            !TryComp<TransformComponent>(ship, out var shipTransform))
+        if (!TryGetPositions(player, ship, out var playerPos, out var shipPos))
             return;
-
-        // Make sure both entities are on the same map
-        if (playerTransform.MapID != shipTransform.MapID)
-            return;
-
-        // Get positions of both entities
-        var playerPos = Transform(player).WorldPosition;
-        var shipPos = Transform(ship).WorldPosition;
 
         // Calculate direction vector
         var direction = shipPos - playerPos;
@@ -47,10 +60,46 @@ public sealed class ShipyardDirectionSystem : EntitySystem
             ("direction", directionName),
             ("distance", distance));
 
+        SendMessageToPlayer(player, message);
+    }
+
+    /// <summary>
+    /// [Null Sector] Sends a provided message to player.
+    /// </summary>
+    private void SendMessageToPlayer(EntityUid player, string message)
+    {
         if (_playerManager.TryGetSessionByEntity(player, out var session))
         {
-            _chatManager.ChatMessageToOne(ChatChannel.Server, message, message, EntityUid.Invalid, false, session.Channel);
+            _chatManager.ChatMessageToOne(ChatChannel.Server,
+                message,
+                message,
+                EntityUid.Invalid,
+                false,
+                session.Channel);
         }
+    }
+
+    /// <summary>
+    /// [Null Sector] Attempts to get the Player and Ship positions from provided Player and Ship Entities.
+    /// </summary>
+    /// <returns>Two Vector2 output-variables: player and ship positions.</returns>
+    private bool TryGetPositions(EntityUid player, EntityUid ship, out Vector2 playerPos, out Vector2 shipPos)
+    {
+        playerPos = Vector2.NaN;
+        shipPos = Vector2.NaN;
+        // Try to get player's and ship's transform components.
+        if (!EntityManager.TryGetComponent<TransformComponent>(player, out var playerTransform) ||
+            !EntityManager.TryGetComponent<TransformComponent>(ship, out var shipTransform))
+            return false;
+
+        // Make sure both entities are on the same map
+        if (playerTransform.MapID != shipTransform.MapID)
+            return false;
+
+        // Get positions of both entities
+        playerPos = _transform.GetWorldPosition(player);
+        shipPos = _transform.GetWorldPosition(ship);
+        return true;
     }
 
     //lua start
